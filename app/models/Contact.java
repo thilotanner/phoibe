b2c7d5e6db8b4f668408509ff2fa0a4de86c65ab
Thilo Tanner <thilo.tanner@gmail.com>
2011-10-26 18:13:09 +0200

    Contacts

--- app/models/Contact.java
+++ app/models/Contact.java
@@ -1,15 +1,42 @@
 package models;
 
+import play.data.validation.Email;
 import play.data.validation.Required;
+import play.data.validation.URL;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 
 @Entity
 public class Contact extends Model {
+
+    public String company;
 
     public String firstName;
 
     @Required
     public String lastName;
+
+    public String street;
+
+    public String postalCode;
+
+    @Required
+    public String city;
+
+    public String phone;
+
+    public String fax;
+
+    public String mobile;
+
+    @Email
+    public String email;
+
+    @URL
+    public String website;
+
+    @Lob
+    public String comments;
 }
---
+++ app/views/AdvancedTags.java
@@ -0,0 +1,65 @@
+package views;
+
+import groovy.lang.Closure;
+import org.apache.commons.lang.StringUtils;
+import play.Play;
+import play.data.validation.Validation;
+import play.mvc.Scope;
+import play.templates.FastTags;
+import play.templates.GroovyTemplate;
+import play.templates.JavaExtensions;
+
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AdvancedTags extends FastTags {
+    /**
+     * The field tag is a helper, based on the spirit of Don't Repeat Yourself.
+     * @param args tag attributes
+     * @param body tag inner body
+     * @param out the output writer
+     * @param template enclosing template
+     * @param fromLine template line number where the tag is defined
+     */
+    public static void _field(Map<?, ?> args, Closure body, PrintWriter out, GroovyTemplate.ExecutableTemplate template, int fromLine) {
+        Map<String,Object> field = new HashMap<String,Object>();
+        String _arg = args.get("arg").toString();
+        field.put("name", _arg);
+        field.put("id", _arg.replace('.', '_'));
+        field.put("flash", Scope.Flash.current().get(_arg));
+        field.put("flashArray", field.get("flash") != null && !StringUtils.isEmpty(field.get("flash").toString()) ? field.get("flash").toString().split(",") : new String[0]);
+        field.put("error", Validation.error(_arg));
+        String errorClass = Play.configuration.getProperty("error.class", "hasError");
+        field.put("errorClass", field.get("error") != null ? errorClass : "");
+        String[] pieces = _arg.split("\\.");
+        Object obj = body.getProperty(pieces[0]);
+        if(obj != null){
+            if(pieces.length > 1){
+                for(int i = 1; i < pieces.length; i++){
+                    try{
+                        Field f = obj.getClass().getField(pieces[i]);
+                        if(i == (pieces.length-1)){
+                            try{
+                                Method getter = obj.getClass().getMethod("get"+ JavaExtensions.capFirst(f.getName()));
+                                field.put("value", getter.invoke(obj, new Object[0]));
+                            }catch(NoSuchMethodException e){
+                                field.put("value",f.get(obj).toString());
+                            }
+                        }else{
+                            obj = f.get(obj);
+                        }
+                    }catch(Exception e){
+                        // if there is a problem reading the field we dont set any value
+                    }
+                }
+            }else{
+                field.put("value", obj);
+            }
+        }
+        body.setProperty("field", field);
+        body.call();
+    }
+}
