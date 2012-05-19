8613ace8ed8b3c04f0df0df8af451659f6ee1b6e
Thilo Tanner <thilo.tanner@gmail.com>
2012-05-19 22:38:30 +0200

    deduplication added

--- app/deduplication/ContactDeduplicator.java
+++ app/deduplication/ContactDeduplicator.java
@@ -1,21 +1,65 @@
 package deduplication;
 
 import models.Contact;
+import no.priv.garshol.duke.Column;
 import no.priv.garshol.duke.ConfigLoader;
 import no.priv.garshol.duke.Configuration;
 import no.priv.garshol.duke.Processor;
+import no.priv.garshol.duke.Property;
 import no.priv.garshol.duke.RecordImpl;
+import no.priv.garshol.duke.comparators.DiceCoefficientComparator;
+import no.priv.garshol.duke.comparators.Levenshtein;
+import no.priv.garshol.duke.comparators.PersonNameComparator;
 import no.priv.garshol.duke.datasources.InMemoryDataSource;
+import no.priv.garshol.duke.datasources.JDBCDataSource;
 import play.Play;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ContactDeduplicator {
 
     public List<Map<Long,Contact>> deduplicate() throws Exception {
-        Configuration config = ConfigLoader.load(Play.getFile("conf/duke-contacts.xml").getAbsolutePath());
-        Processor processor = new Processor(config);
+        Configuration configuration = new Configuration();
+        configuration.setThreshold(0.9057339039936381d);
+        configuration.setMaybeThreshold(0.9057339039936381d);
+
+        JDBCDataSource dataSource = new JDBCDataSource();
+        dataSource.setDriverClass(Play.configuration.getProperty("db.driver"));
+        dataSource.setConnectionString(Play.configuration.getProperty("db.url"));
+        dataSource.setUserName(Play.configuration.getProperty("db.user"));
+        dataSource.setPassword(Play.configuration.getProperty("db.pass"));
+        dataSource.setQuery("select id,company,firstName,lastName,street,city from Contact");
+        dataSource.addColumn(new Column("id", "id", null, null));
+        dataSource.addColumn(new Column("company", "company", null, null));
+        dataSource.addColumn(new Column("firstName", "firstName", null, null));
+        dataSource.addColumn(new Column("lastName", "lastName", null, null));
+        dataSource.addColumn(new Column("street", "street", null, null));
+        dataSource.addColumn(new Column("city", "city", null, null));
+        configuration.addDataSource(0, dataSource);
+
+        List<Property> properties = new ArrayList<Property>();
+        properties.add(new Property("id"));
+
+        Property company = new Property("company", new Levenshtein(), 0.46558278093899846d, 0.980293137592496d);
+        properties.add(company);
+
+        Property firstName = new Property("firstName", new Levenshtein(), 0.3894780895403918d, 0.737258653002584d);
+        properties.add(firstName);
+
+        Property lastName = new Property("lastName", new PersonNameComparator(), 0.33473200176209683d, 0.6895391273091356d);
+        properties.add(lastName);
+
+        Property street = new Property("street", new Levenshtein(), 0.030475349613534597d, 0.7644584183350849d);
+        properties.add(street);
+
+        Property city = new Property("city", new DiceCoefficientComparator(), 0.4590544044617058d, 0.6784963225856417d);
+        properties.add(city);
+
+        configuration.setProperties(properties);
+
+        Processor processor = new Processor(configuration);
         MapMatchListener<Contact> listener = new MapMatchListener<Contact>(Contact.class);
         processor.addMatchListener(listener);
         processor.deduplicate();
